9d974a567ca905d5536d9d2ea08ca7cfb5fdaa81
Tora77 <kiku.happy95@gmail.com>
2026-01-08 12:14:22 -0800

    選択肢のデータを作成

--- AnimalQuiz/QuizView.swift
+++ AnimalQuiz/QuizView.swift
@@ -17,6 +17,42 @@ struct QuizItem{
 struct QuizView: View {
     @State var isShowingScoreView = false
     let choices = ["ライオン", "ウサインボルト" ,"チーター", "馬"]
+//    let quizItem = QuizItem(
+//        questuon: "次のうち、世界で最も速く走る動物はどれですか？",
+//        choices: ["ライオン", "ウサインボルト" ,"チーター", "馬"],
+//        correctAnswer: "チーター"
+//    )
+    let quizItems = [
+        QuizItem(
+            questuon: "次のうち、世界で最も速く走る動物はどれですか？",
+            choices: ["ライオン", "ウサインボルト" ,"チーター", "ウマ"],
+            correctAnswer: "チーター"
+        ),
+        QuizItem(
+            questuon: "次のうち、飛ぶことができない鳥はどれですか？",
+            choices: ["ペンギン", "フクロウ" ,"ハト", "スズメ"],
+            correctAnswer: "ペンギン"
+        ),
+        QuizItem(
+            questuon: "次のうち、哺乳類ではない動物はどれですか？",
+            choices: ["イルカ", "カメ" ,"コウモリ", "ヒト"],
+            correctAnswer: "カメ"
+        ),
+        QuizItem(
+            questuon: "次のうち、夜行性ではない動物はどれですか？",
+            choices: ["ライオン", "コアラ" ,"ゾウ", "フクロウ"],
+            correctAnswer: "ゾウ"
+        ),
+        QuizItem(
+            questuon: "次のうち、最も長い首を持つ動物はどれですか？",
+            choices: ["キリン", "アルパカ" ,"ゾウ", "ウマ"],
+            correctAnswer: "キリン"
+        )
+       
+        
+    ]
+    
+    
     
     var body: some View {
        VStack {
@@ -87,6 +123,31 @@ struct QuizView: View {
 //struct QuizView:View {
 //    @State var isShowingScoreView = false
 //    let choices = ["ライオン", "ウサイン・ボルト", "チーター", "馬"]
+//    
+//    let quizeItems = [
+//        QuizItem(
+//            question: "次のうち、世界で最も速く走る動物はどれですか？",
+//            choices: ["ライオン", "ウサイン・ボルト", "チーター", "馬"],
+//            correctAnswer: "チーター"
+//        ), QuizItem(
+//            question: "次のうち、飛ぶことができない鳥はどれですか？",
+//            choices: ["ペンギン", "フクロウ" ,"ハト", "スズメ"],
+//            correctAnswer: "ペンギン"
+//        ), QuizItem(
+//            question: "次のうち、哺乳類ではない動物はどれですか？",
+//            choices: ["イルカ", "カメ" ,"コウモリ", "ヒト"],
+//            correctAnswer: "カメ"
+//        ), QuizItem(
+//            question: "次のうち、夜行性ではない動物はどれですか？",
+//            choices: ["ライオン", "コアラ" ,"ゾウ", "フクロウ"],
+//            correctAnswer: "ゾウ"
+//        ), QuizItem(
+//            question: "次のうち、最も長い首を持つ動物はどれですか？",
+//            choices: ["キリン", "アルパカ" ,"ゾウ", "ウマ"],
+//            correctAnswer: "キリン"
+//        ),
+//        
+//    ]
 //    
 //    var body: some View {
 //        VStack{
