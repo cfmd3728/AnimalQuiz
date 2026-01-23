84b3d06ebd2208b4ef66ecc08b780b14d0e247f1
Tora77 <kiku.happy95@gmail.com>
2026-01-23 09:49:25 -0800

    クイズ選択画面の作成

---
+++ AnimalQuiz/Views/GenreSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  GenreSelectionView.swift
+//  AnimalQuiz
+//
+//  Created by 菊池和広 on 2026/01/22.
+//
+
+import SwiftUI
+
+struct GenreSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenreSelectionView()
+}
